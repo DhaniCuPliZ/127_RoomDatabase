3b83d3e1bcc1fd6d26d4d510a37c9e2b6f316fad
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-11-27 16:30:10 +0700

    Membuat ContainerDataApp untuk inisialisasi repository

--- app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
@@ -1,5 +1,18 @@
 package com.example.roomdatabase.repositori
 
+import android.content.Context
+import com.example.roomdatabase.room.DatabaseSiswa
+
 interface ContainerApp {
     val repositorySiswa : RepositorySiswa
 }
+
+class ContainerDataApp(private val context: Context):
+        ContainerApp{
+            override val repositorySiswa: RepositorySiswa by lazy {
+                OfflineRepositoriSiswa(
+                    siswaDao = DatabaseSiswa.getDatabase(context).siswaDao()
+                )
+            }
+        }
+
