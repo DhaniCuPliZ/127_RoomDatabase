cac4f3090d75b0db45a9b49f6f35f836c7baa6be
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-11-27 16:34:37 +0700

    Menambahkan class AplikasiSiswa untuk inisialisasi containe

--- app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
@@ -3,7 +3,7 @@ package com.example.roomdatabase.repositori
 import com.example.roomdatabase.room.Siswa
 import kotlinx.coroutines.flow.Flow
 
-interface RepositorySiswa {
+interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
 }
