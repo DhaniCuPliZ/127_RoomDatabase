c3da39de6f6ed4af0b3e29fa1ea84087fb4a3a0d
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-11-27 16:49:27 +0700

    Menambahkan data class DetailSiswa

---
+++ app/src/main/java/com/example/roomdatabase/viewmodel/EntryViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.roomdatabase.viewmodel
+
+class EntryViewModel {
+}
+
+data class  DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
