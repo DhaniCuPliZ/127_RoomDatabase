04bd7432eef4fbfc09d412aecb0943c2dc669605
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-11-27 16:36:48 +0700

    perbaikan huruf SiswaDao

--- app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
@@ -7,7 +7,7 @@ import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface SiswaDAO {
+interface SiswaDao {
     @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa() : Flow<List<Siswa>>
 
